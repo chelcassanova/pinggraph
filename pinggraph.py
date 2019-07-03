f17dca43eadb525dde4dffc781666af3543158e3
chelcassanova <sassychels251@gmail.com>
2019-07-02 23:37:08 -0400

    It works, added functionality so that the graph scrolls

--- pinggraph.py
+++ pinggraph.py
@@ -4,6 +4,7 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import sys
 from ping import Ping
 
 ping = Ping()
@@ -15,13 +16,38 @@ fig.suptitle('AAAAAAAAAAAAAAAAAA')  # Give it a main title
 ax = plt.subplot()  # Create an Axes object for the figure
 ax.set_xlabel('wrgg')  # Set the label for the x-axis
 ax.set_ylabel('Response Time (ms)')  # Set the label for the y-axis
-plt.ion()
+# plt.ion()
 
 pingarray = []
 
-def animate():
-    pingarray.append(ping.call(1))
-    plt.plot(pingarray)
+# PROBLEM: Cross-platform program only works on Windows
+# TODO: Get graph to be one colour
+def animate(i, ostype=0):  # I don't know why, but the func def needs an argument
+    pingresponse = ping.call(ostype)  # Get the response time of the ping call
+    pingarray.append(pingresponse)  # Add that response time to a list of responses for plotting
+    plt.plot(pingarray)  # Plot the current array
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
-plt.show()
+    # If there are more than 20 items in the array
+    # then clear the current axes, remove the very first item
+    # and plot again. This is what makes the graph look like it's "scrolling"
+    if len(pingarray) > 20:
+        plt.cla()
+        pingarray.pop(0)
+        plt.plot(pingarray)
+
+    # Update the y-axis label with the current ping value
+    ax.set_ylabel(str.format('Response Time (ms): {}', pingresponse))
+
+
+def main():
+    # If this is being run on Windows, set the "os type" to 0, otherwise, set it to 1
+    if sys.platform == "win32":
+        osint = 0
+    else:
+        osint = 1
+    ani = animation.FuncAnimation(fig, animate, fargs=(osint), interval=500)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
